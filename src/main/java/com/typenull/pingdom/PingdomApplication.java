9bd504fe5389acccae0e3d57a7ac547483a9d2a9
김우성 <azunox9@gmail.com>
2026-04-09 20:16:19 +0900

    feat : 프로젝트 초기 환경 세팅

---
+++ src/main/java/com/typenull/pingdom/PingdomApplication.java
@@ -0,0 +1,13 @@
+package com.typenull.pingdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingdomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingdomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/typenull/pingdom/PingdomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typenull.pingdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingdomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
