81bb2c190cd6a43328c6fef9c9c3f7129762ebbb
김우성 <azunox9@gmail.com>
2026-04-14 21:48:55 +0900

    feat : 이메일 인증 서비스 로직 추가

--- src/main/java/com/typenull/pingdom/domain/auth/service/AuthService.java
+++ src/main/java/com/typenull/pingdom/domain/auth/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.typenull.pingdom.domain.auth.service;
 
+import com.typenull.pingdom.domain.auth.dto.email.EmailVerifyRequest;
 import com.typenull.pingdom.domain.auth.dto.login.LoginRequest;
 import com.typenull.pingdom.domain.auth.dto.login.LoginResponse;
 import com.typenull.pingdom.domain.auth.dto.signup.SignupRequest;
@@ -9,4 +10,7 @@ public interface AuthService {
     public UserResponse signup(SignupRequest request);
 
     public LoginResponse login(LoginRequest request);
+
+    // 이메일 인증 처리 메서드
+    public void verifyEmail(EmailVerifyRequest request);
 }
--- src/main/java/com/typenull/pingdom/domain/auth/service/impl/AuthServiceImpl.java
+++ src/main/java/com/typenull/pingdom/domain/auth/service/impl/AuthServiceImpl.java
@@ -1,6 +1,7 @@
 package com.typenull.pingdom.domain.auth.service.impl;
 
 import com.typenull.pingdom.domain.auth.domain.User;
+import com.typenull.pingdom.domain.auth.dto.email.EmailVerifyRequest;
 import com.typenull.pingdom.domain.auth.dto.login.LoginRequest;
 import com.typenull.pingdom.domain.auth.dto.login.LoginResponse;
 import com.typenull.pingdom.domain.auth.dto.signup.SignupRequest;
@@ -50,5 +51,16 @@ public class AuthServiceImpl implements AuthService {
         }
 
         return new LoginResponse(user.getId(), user.getUsername(), user.getName(), "로그인에 성공했습니다.");
+    }
+
+    @Override
+    @Transactional
+    // 이메일 기준 사용자 인증 처리 메서드
+    public void verifyEmail(EmailVerifyRequest request) {
+        User user = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new AuthException(AuthErrorCode.USER_NOT_FOUND));
+
+        // 이메일 인증 상태 반영 호출
+        user.verifyEmail();
     }
 }
