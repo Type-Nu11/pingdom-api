29c9f3a2bf1f8f6b7c017a0d85133aa311e480a0
yongin1212 <godghr10031@gmail.com>
2026-05-06 20:13:21 +0900

    chore : aws의존성 버전업

--- src/main/java/com/typenull/pingdom/domain/map/service/S3Service.java
+++ src/main/java/com/typenull/pingdom/domain/map/service/S3Service.java
@@ -14,8 +14,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.util.UUID;
 
 @Service
