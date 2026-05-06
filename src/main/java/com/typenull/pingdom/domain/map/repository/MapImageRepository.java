41d2fdae66988bde6309e09e81845420b9cc0fbe
yongin1212 <godghr10031@gmail.com>
2026-05-06 16:48:51 +0900

    feat : dto 추가

--- src/main/java/com/typenull/pingdom/domain/map/repository/MapImageRepository.java
+++ src/main/java/com/typenull/pingdom/domain/map/repository/MapImageRepository.java
@@ -2,6 +2,8 @@ package com.typenull.pingdom.domain.map.repository;
 
 import com.typenull.pingdom.domain.map.domain.MapImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MapImageRepository extends JpaRepository<MapImage,Long> {
 }
