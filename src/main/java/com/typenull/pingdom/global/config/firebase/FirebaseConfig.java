9d4e580a61d2aad4fdecd3f824c906211fd7685f
yongin1212 <godghr10031@gmail.com>
2026-05-19 17:25:10 +0900

    refactor : 재미나이 리뷰반영, 로컬 컴포즈 추가, 주석 추가

--- src/main/java/com/typenull/pingdom/global/config/firebase/FirebaseConfig.java
+++ src/main/java/com/typenull/pingdom/global/config/firebase/FirebaseConfig.java
@@ -4,7 +4,6 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
