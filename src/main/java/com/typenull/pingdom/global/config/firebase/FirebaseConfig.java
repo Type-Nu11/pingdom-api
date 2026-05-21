9f74d0be19ffa30a345ca55a60b75c15de52cbbb
yongin1212 <godghr10031@gmail.com>
2026-05-21 21:47:22 +0900

    refactor : json을 받는 형식으로 변경

--- src/main/java/com/typenull/pingdom/global/config/firebase/FirebaseConfig.java
+++ src/main/java/com/typenull/pingdom/global/config/firebase/FirebaseConfig.java
@@ -3,31 +3,29 @@ package com.typenull.pingdom.global.config.firebase;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import java.io.ByteArrayInputStream;
+import org.springframework.core.io.ClassPathResource;
+
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
 
 @Configuration
 @Slf4j
 public class FirebaseConfig {
 
-    @PostConstruct
-    public void init() throws IOException {
-        String fcmKeyJson = System.getenv("FCM_KEY_JSON");
-        if (fcmKeyJson == null || fcmKeyJson.isEmpty()) {
-            throw new IllegalStateException("FCM_KEY_JSON 환경변수가 없습니다.");
-        }
-        fcmKeyJson = fcmKeyJson.replace("\\n", "\n");
-        InputStream stream = new ByteArrayInputStream(fcmKeyJson.getBytes(StandardCharsets.UTF_8));
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        InputStream stream = new ClassPathResource("fcm-key.json").getInputStream();
+
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(stream))
                 .build();
+
         if (FirebaseApp.getApps().isEmpty()) {
-            FirebaseApp.initializeApp(options);
+            return FirebaseApp.initializeApp(options);
         }
+        return FirebaseApp.getInstance();
     }
 }
