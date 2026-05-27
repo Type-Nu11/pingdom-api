15155ba4323c94de9dbdf6b98d38e5357a2312a6
shawnatak926 <poomepoo90@gmail.com>
2026-05-27 19:51:05 +0900

    chore : dev profile에서 FCM bootstrap 비활성화

--- src/main/java/com/typenull/pingdom/global/config/firebase/FirebaseConfig.java
+++ src/main/java/com/typenull/pingdom/global/config/firebase/FirebaseConfig.java
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -20,6 +21,7 @@ public class FirebaseConfig {
     private String fcmKeyPath;
 
     @Bean
+    @ConditionalOnProperty(name = "fcm.enabled", havingValue = "true", matchIfMissing = true)
     public FirebaseApp firebaseApp() throws IOException {
         try (InputStream stream = new FileInputStream(fcmKeyPath)) {
             FirebaseOptions options = FirebaseOptions.builder()
