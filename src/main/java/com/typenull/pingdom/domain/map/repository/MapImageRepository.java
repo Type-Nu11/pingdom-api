e692c7b419527c121fb9944c3ed7fb0f39849689
김우성 <azunox9@gmail.com>
2026-05-28 17:28:46 +0900

    feat : 장소별 게시글 조회용 리포지토리 메서드 추가

--- src/main/java/com/typenull/pingdom/domain/map/repository/MapImageRepository.java
+++ src/main/java/com/typenull/pingdom/domain/map/repository/MapImageRepository.java
@@ -1,6 +1,7 @@
 package com.typenull.pingdom.domain.map.repository;
 
 import com.typenull.pingdom.domain.map.domain.MapImage;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -17,4 +18,6 @@ public interface MapImageRepository extends JpaRepository<MapImage,Long> {
     WHERE m.id = :imageId
 """)
     void increaseLikeCount(@Param("imageId") Long imageId);
+
+    List<MapImage> findByMapPlace_IdOrderByCreatedAtDesc(Long placeId);
 }
