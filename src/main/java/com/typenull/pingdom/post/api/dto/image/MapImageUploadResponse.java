a13528b8c813653ce8fcc4aa9522b42733afe7ba
김우성 <azunox9@gmail.com>
2026-06-05 10:53:06 +0900

    refactor : 미사용 api dto 제거

--- src/main/java/com/typenull/pingdom/post/api/dto/image/MapImageUploadResponse.java
+++
@@ -1,9 +0,0 @@
-package com.typenull.pingdom.post.api.dto.image;
-
-public record MapImageUploadResponse(
-        Long id,
-        String imageUrl,
-        String s3Key,
-        String message
-) {
-}
