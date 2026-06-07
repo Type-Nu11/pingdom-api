13901c259081c2b569370fb9fd2e84c07a6f19cf
shawnatak926 <poomepoo90@gmail.com>
2026-06-07 15:01:29 +0900

    fix : 북마크 목록 조회 count 쿼리 정합성 수정

--- src/main/java/com/typenull/pingdom/place/infrastructure/persistence/MapPlaceRepository.java
+++ src/main/java/com/typenull/pingdom/place/infrastructure/persistence/MapPlaceRepository.java
@@ -104,6 +104,7 @@ public interface MapPlaceRepository extends JpaRepository<MapPlace, Long> {
             countQuery = """
                     SELECT COUNT(b)
                     FROM MapBookmark b
+                    JOIN MapPlace p ON p.id = b.placeId
                     WHERE b.userId = :userId
                     """
     )
