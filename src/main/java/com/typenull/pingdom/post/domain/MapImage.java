d40e9089017c62e13312ce47ec6f461af07f2094
김우성 <azunox9@gmail.com>
2026-06-22 11:51:26 +0900

    chore : 게시글 제목 필수 제약 마이그레이션 추가

--- src/main/java/com/typenull/pingdom/post/domain/MapImage.java
+++ src/main/java/com/typenull/pingdom/post/domain/MapImage.java
@@ -33,7 +33,7 @@ public class MapImage {
     @Column(name = "s3_key", length = 500, nullable = false)
     private String s3Key;
 
-    @Column(name = "title", length = 100)
+    @Column(name = "title", length = 100, nullable = false)
     private String title;
 
     @Column(name = "description", length = 1000)
