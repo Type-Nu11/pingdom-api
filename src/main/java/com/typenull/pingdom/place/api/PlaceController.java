cf9b2fac086cd71c7570c90b82621f84b7e282ed
김우성 <azunox9@gmail.com>
2026-06-23 14:59:22 +0900

    refactor : place principal import 갱신

--- src/main/java/com/typenull/pingdom/place/api/PlaceController.java
+++ src/main/java/com/typenull/pingdom/place/api/PlaceController.java
@@ -10,7 +10,7 @@ import com.typenull.pingdom.place.application.service.recommendation.PlaceRecomm
 import com.typenull.pingdom.place.application.service.place.PlaceQueryService;
 import com.typenull.pingdom.place.application.service.place.PlaceSearchCondition;
 import com.typenull.pingdom.place.application.service.recommendation.PlaceRecommendationQueryService;
-import com.typenull.pingdom.shared.security.JwtAuthenticatedUser;
+import com.typenull.pingdom.shared.security.principal.JwtAuthenticatedUser;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
