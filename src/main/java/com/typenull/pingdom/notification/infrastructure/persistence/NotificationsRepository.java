eccb1618b07f6ce0a619eb4ec83e08eeb5dd0ae1
김우성 <azunox9@gmail.com>
2026-06-23 15:00:43 +0900

    refactor : notification repository 패키지 이동

--- src/main/java/com/typenull/pingdom/notification/infrastructure/persistence/NotificationsRepository.java
+++ src/main/java/com/typenull/pingdom/notification/infrastructure/persistence/NotificationsRepository.java
@@ -1,4 +1,4 @@
-package com.typenull.pingdom.notification.repository;
+package com.typenull.pingdom.notification.infrastructure.persistence;
 
 import com.typenull.pingdom.notification.domain.Notifications;
 import org.springframework.data.jpa.repository.JpaRepository;
