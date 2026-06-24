e57d79fcc43bf4de6edcea19a2a78392d5e31006
yongin1212 <godghr10031@gmail.com>
2026-06-24 09:33:07 +0900

    feat : 의심도 점수

--- src/main/java/com/typenull/pingdom/engagement/event/PostReportCreatedEventListener.java
+++ src/main/java/com/typenull/pingdom/engagement/event/PostReportCreatedEventListener.java
@@ -11,6 +11,7 @@ import com.typenull.pingdom.moderation.domain.exception.AdminException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -19,6 +20,7 @@ public class PostReportCreatedEventListener {
     private final PostReportRepository postReportRepository;
     private final UserRepository userRepository;
 
+    @Transactional
     @EventListener
     public void handle(PostReportCreatedEvent event) {
         Long postReportId = event.postReportId();
@@ -33,5 +35,18 @@ public class PostReportCreatedEventListener {
             report.increaseReportScore(10);
         }
 
+        // 최근 3개 신고 사유 복붙 체크
+        if (postReportRepository.existsSameReasonInLatestThreeBeforeCurrent(
+                report.getReporterUserId(),
+                report.getId(),
+                report.getReason())
+        ){
+            report.increaseReportScore(15);
+        }
+
+        // 10명 이상의 사람이 같은 이미지를 신고했다면
+        if (postReportRepository.existsAtLeastTenDistinctReportersByReportedImageId(report.getReportedImageId())){
+            report.decreaseReportScore(15);
+        }
     }
 }
