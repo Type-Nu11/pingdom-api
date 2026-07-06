63099d103d6dd2bb6527b8cca30a50ab446cc99e
김우성 <azunox9@gmail.com>
2026-07-07 01:29:32 +0900

    fix : 인증 플로우 rate limit 정책 추가

--- src/main/java/com/typenull/pingdom/shared/ratelimit/RateLimitAction.java
+++ src/main/java/com/typenull/pingdom/shared/ratelimit/RateLimitAction.java
@@ -1,9 +1,11 @@
 package com.typenull.pingdom.shared.ratelimit;
 
 public enum RateLimitAction {
+    SIGNUP,
     LOGIN,
     TOKEN_REFRESH,
     EMAIL_RESEND,
+    EMAIL_VERIFY,
     PASSWORD_RESET_REQUEST,
     PASSWORD_RESET_CONFIRM,
     POST_REPORT,
