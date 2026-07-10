f7247dee7fd16153a538133d561c4227008d02f3
김우성 <azunox9@gmail.com>
2026-07-10 13:41:12 +0900

    feat : 관광객 탐색용 장소 도메인 모델 확장

---
+++ src/main/java/com/typenull/pingdom/place/domain/place/TouristCategory.java
@@ -0,0 +1,13 @@
+package com.typenull.pingdom.place.domain.place;
+
+public enum TouristCategory {
+    K_POP,
+    BEAUTY,
+    FASHION,
+    CAFE,
+    FOOD,
+    POP_UP,
+    EXHIBITION,
+    NIGHTLIFE,
+    OTHER
+}
