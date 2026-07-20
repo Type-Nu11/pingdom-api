75ddad6c3ee6a791dbca395e3c8fb30ee4c2ea1b
김우성 <azunox9@gmail.com>
2026-07-20 20:55:40 +0900

    fix : Flyway 마이그레이션 버전 충돌 해소

--- src/test/java/com/typenull/pingdom/migration/FlywayMigrationIntegrationTest.java
+++ src/test/java/com/typenull/pingdom/migration/FlywayMigrationIntegrationTest.java
@@ -67,8 +67,8 @@ class FlywayMigrationIntegrationTest {
         MigrateResult result = migrate(false);
 
         assertThat(result.success).isTrue();
-        assertThat(result.targetSchemaVersion).isEqualTo("54");
-        assertThat(result.migrationsExecuted).isEqualTo(54);
+        assertThat(result.targetSchemaVersion).isEqualTo("55");
+        assertThat(result.migrationsExecuted).isEqualTo(55);
 
         assertPostMigrationSchema();
     }
@@ -80,8 +80,8 @@ class FlywayMigrationIntegrationTest {
         MigrateResult result = migrate(true);
 
         assertThat(result.success).isTrue();
-        assertThat(result.targetSchemaVersion).isEqualTo("54");
-        assertThat(result.migrationsExecuted).isEqualTo(53);
+        assertThat(result.targetSchemaVersion).isEqualTo("55");
+        assertThat(result.migrationsExecuted).isEqualTo(54);
 
         try (Connection connection = postgres.createConnection("");
              Statement statement = connection.createStatement()) {
@@ -123,8 +123,8 @@ class FlywayMigrationIntegrationTest {
         MigrateResult result = migrate(false);
 
         assertThat(result.success).isTrue();
-        assertThat(result.targetSchemaVersion).isEqualTo("54");
-        assertThat(result.migrationsExecuted).isEqualTo(27);
+        assertThat(result.targetSchemaVersion).isEqualTo("55");
+        assertThat(result.migrationsExecuted).isEqualTo(28);
         try (Connection connection = postgres.createConnection("");
              Statement statement = connection.createStatement()) {
             assertThat(queryBoolean(statement, """
