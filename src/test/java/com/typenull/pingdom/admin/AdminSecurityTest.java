f174f418b3137557d8f9e1d7b5ed5703134ea09f
shawnatak926 <poomepoo90@gmail.com>
2026-07-20 16:19:27 +0900

    test : audit targetType 응답 검증 추가

--- src/test/java/com/typenull/pingdom/admin/AdminSecurityTest.java
+++ src/test/java/com/typenull/pingdom/admin/AdminSecurityTest.java
@@ -165,7 +165,9 @@ class AdminSecurityTest {
                         .param("to", "2026-07-01T23:59:59"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.auditLogs[0].action").value(AdminAuditAction.REPORT_ACCEPTED.name()))
-                .andExpect(jsonPath("$.auditLogs[1].action").value(AdminAuditAction.USER_BAN_APPLIED.name()));
+                .andExpect(jsonPath("$.auditLogs[0].targetType").value(AdminAuditTargetType.REPORT.name()))
+                .andExpect(jsonPath("$.auditLogs[1].action").value(AdminAuditAction.USER_BAN_APPLIED.name()))
+                .andExpect(jsonPath("$.auditLogs[1].targetType").value(AdminAuditTargetType.USER.name()));
     }
 
     private SecurityRegressionFixture securityRegressionFixture() throws Exception {
