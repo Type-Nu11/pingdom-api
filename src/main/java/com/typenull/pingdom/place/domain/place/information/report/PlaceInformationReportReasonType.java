3f97dfa5223296a9165e28ba1cd3549783f43b16
yongin1212 <godghr10031@gmail.com>
2026-07-21 10:38:02 +0900

    feat : 장소 정보 신고 반박 도메인 모델 추가

---
+++ src/main/java/com/typenull/pingdom/place/domain/place/information/report/PlaceInformationReportReasonType.java
@@ -0,0 +1,12 @@
+package com.typenull.pingdom.place.domain.place.information.report;
+
+public enum PlaceInformationReportReasonType {
+    INCORRECT,
+    OUTDATED,
+    MISSING,
+    MISLEADING,
+    DUPLICATE,
+    CLOSED_OR_MOVED,
+    SPAM_OR_ABUSE,
+    OTHER
+}
