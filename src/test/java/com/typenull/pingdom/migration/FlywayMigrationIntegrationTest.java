f7d882102b1461243bd0d1093f5129efdc3b5663
김우성 <azunox9@gmail.com>
2026-07-21 10:22:40 +0900

    fix : Flyway 증빙 마이그레이션 버전 충돌 해결

--- src/test/java/com/typenull/pingdom/migration/FlywayMigrationIntegrationTest.java
+++ src/test/java/com/typenull/pingdom/migration/FlywayMigrationIntegrationTest.java
@@ -67,8 +67,8 @@ class FlywayMigrationIntegrationTest {
         MigrateResult result = migrate(false);
 
         assertThat(result.success).isTrue();
-        assertThat(result.targetSchemaVersion).isEqualTo("56");
-        assertThat(result.migrationsExecuted).isEqualTo(56);
+        assertThat(result.targetSchemaVersion).isEqualTo("57");
+        assertThat(result.migrationsExecuted).isEqualTo(57);
 
         assertPostMigrationSchema();
     }
@@ -80,7 +80,7 @@ class FlywayMigrationIntegrationTest {
         MigrateResult result = migrate(true);
 
         assertThat(result.success).isTrue();
-        assertThat(result.targetSchemaVersion).isEqualTo("56");
+        assertThat(result.targetSchemaVersion).isEqualTo("57");
         assertThat(result.migrationsExecuted).isEqualTo(55);
 
         try (Connection connection = postgres.createConnection("");
@@ -123,7 +123,7 @@ class FlywayMigrationIntegrationTest {
         MigrateResult result = migrate(false);
 
         assertThat(result.success).isTrue();
-        assertThat(result.targetSchemaVersion).isEqualTo("56");
+        assertThat(result.targetSchemaVersion).isEqualTo("57");
         assertThat(result.migrationsExecuted).isEqualTo(29);
         try (Connection connection = postgres.createConnection("");
              Statement statement = connection.createStatement()) {
@@ -309,7 +309,7 @@ class FlywayMigrationIntegrationTest {
         MigrateResult result = migrate(false);
 
         assertThat(result.success).isTrue();
-        assertThat(result.targetSchemaVersion).isEqualTo("56");
+        assertThat(result.targetSchemaVersion).isEqualTo("57");
         assertThat(result.migrationsExecuted).isEqualTo(1);
         try (Connection connection = postgres.createConnection("");
              Statement statement = connection.createStatement()) {
