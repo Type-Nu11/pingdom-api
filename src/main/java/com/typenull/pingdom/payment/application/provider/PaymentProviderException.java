de553b8c7b96d11fef269c667d900a6e12f171fc
김우성 <azunox9@gmail.com>
2026-07-26 09:06:14 +0900

    fix : Provider 실패 확정 여부 구분

--- src/main/java/com/typenull/pingdom/payment/application/provider/PaymentProviderException.java
+++ src/main/java/com/typenull/pingdom/payment/application/provider/PaymentProviderException.java
@@ -5,9 +5,11 @@ import lombok.Getter;
 @Getter
 public class PaymentProviderException extends RuntimeException {
     private final String failureCode;
+    private final PaymentProviderFailureType failureType;
 
-    public PaymentProviderException(String failureCode, String message) {
+    public PaymentProviderException(PaymentProviderFailureType failureType, String failureCode, String message) {
         super(message);
+        this.failureType = failureType;
         this.failureCode = failureCode;
     }
 }
