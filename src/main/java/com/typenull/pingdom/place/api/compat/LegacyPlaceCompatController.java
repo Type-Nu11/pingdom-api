4a7b6554442bae1e0f63cc5160a8212f46c5f3ba
yongin1212 <godghr10031@gmail.com>
2026-08-01 19:49:10 +0900

    refactor : 장소 호환 컨트롤러 패키지 이관

--- src/main/java/com/typenull/pingdom/place/api/compat/LegacyPlaceCompatController.java
+++ src/main/java/com/typenull/pingdom/place/api/compat/LegacyPlaceCompatController.java
@@ -1,4 +1,4 @@
-package com.typenull.pingdom.place.api;
+package com.typenull.pingdom.place.api.compat;
 
 import com.typenull.pingdom.place.api.dto.coordinate.PlaceCoordinateCreateRequest;
 import com.typenull.pingdom.place.api.dto.coordinate.PlaceCoordinateCreateResponse;
