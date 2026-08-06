ca6ebde09fb421aed716657caac18a0ffb7eccef
shawnatak926 <poomepoo90@gmail.com>
2026-08-06 16:47:48 +0900

    test : Flyway migration 실행 개수 기준 갱신

--- src/test/java/com/typenull/pingdom/migration/FlywayMigrationIntegrationTest.java
+++ src/test/java/com/typenull/pingdom/migration/FlywayMigrationIntegrationTest.java
@@ -207,7 +207,7 @@ class FlywayMigrationIntegrationTest {
 
         assertThat(result.success).isTrue();
         assertThat(result.targetSchemaVersion).isEqualTo(LATEST_MIGRATION_VERSION);
-        assertThat(result.migrationsExecuted).isEqualTo(85);
+        assertThat(result.migrationsExecuted).isEqualTo(86);
 
         try (Connection connection = postgres.createConnection("");
              Statement statement = connection.createStatement()) {
@@ -321,7 +321,7 @@ class FlywayMigrationIntegrationTest {
 
         assertThat(result.success).isTrue();
         assertThat(result.targetSchemaVersion).isEqualTo(LATEST_MIGRATION_VERSION);
-        assertThat(result.migrationsExecuted).isEqualTo(60);
+        assertThat(result.migrationsExecuted).isEqualTo(61);
         try (Connection connection = postgres.createConnection("");
              Statement statement = connection.createStatement()) {
             assertThat(queryBoolean(statement, """
@@ -572,7 +572,7 @@ class FlywayMigrationIntegrationTest {
 
         assertThat(result.success).isTrue();
         assertThat(result.targetSchemaVersion).isEqualTo(LATEST_MIGRATION_VERSION);
-        assertThat(result.migrationsExecuted).isEqualTo(32);
+        assertThat(result.migrationsExecuted).isEqualTo(33);
         try (Connection connection = postgres.createConnection("");
              Statement statement = connection.createStatement()) {
             assertThat(queryBoolean(statement, """
