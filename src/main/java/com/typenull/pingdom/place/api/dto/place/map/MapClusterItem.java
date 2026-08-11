9312af7d9fdb2853915cb8c9a6d317799a17c31a
김우성 <azunox9@gmail.com>
2026-08-11 23:13:09 +0900

    docs : 지도 viewport 응답 계약 보강

--- src/main/java/com/typenull/pingdom/place/api/dto/place/map/MapClusterItem.java
+++ src/main/java/com/typenull/pingdom/place/api/dto/place/map/MapClusterItem.java
@@ -1,9 +1,15 @@
 package com.typenull.pingdom.place.api.dto.place.map;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record MapClusterItem(
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
         String clusterId,
+        @Schema(minimum = "-90.0", maximum = "90.0", requiredMode = Schema.RequiredMode.REQUIRED)
         double latitude,
+        @Schema(minimum = "-180.0", maximum = "180.0", requiredMode = Schema.RequiredMode.REQUIRED)
         double longitude,
+        @Schema(requiredMode = Schema.RequiredMode.REQUIRED)
         long placeCount
 ) {
 }
