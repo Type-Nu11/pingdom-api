71d7063fd426732a056bb0ed4c0a1cf00d1c32f4
yongin1212 <godghr10031@gmail.com>
2026-08-14 23:47:43 +0900

    test : 장소 삭제 서비스 책임 검증 추가

--- src/test/java/com/typenull/pingdom/moderation/application/service/place/AdminPlaceServiceSplitTest.java
+++ src/test/java/com/typenull/pingdom/moderation/application/service/place/AdminPlaceServiceSplitTest.java
@@ -2,6 +2,8 @@ package com.typenull.pingdom.moderation.application.service.place;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.typenull.pingdom.moderation.application.service.place.quality.AdminMapPlaceService;
+import com.typenull.pingdom.moderation.application.service.place.merge.AdminPlaceMergeService;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.util.Arrays;
@@ -9,6 +11,18 @@ import java.util.List;
 
 /** 기능별 구현이 물리적으로 분리된 상태를 고정하는 구조 회귀 테스트다. */
 class AdminPlaceServiceSplitTest {
+
+    @org.junit.jupiter.api.Test
+    void mapPlaceServiceRetainsOnlyDeletionUseCase() {
+        assertThat(publicMethodNames(AdminMapPlaceService.class))
+                .containsExactly("deletePlace");
+    }
+
+    @org.junit.jupiter.api.Test
+    void mergeServiceOwnsMergeHistoryAndRestoreUseCases() {
+        assertThat(publicMethodNames(AdminPlaceMergeService.class))
+                .containsExactly("listMergeHistories", "mergePlaces", "restoreMerge");
+    }
 
     private List<String> publicMethodNames(Class<?> serviceType) {
         return Arrays.stream(serviceType.getDeclaredMethods())
