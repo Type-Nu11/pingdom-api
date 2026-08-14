57e71b08ed3334a2cce480a414452b6aab39ebcf
yongin1212 <godghr10031@gmail.com>
2026-08-14 23:48:15 +0900

    test : 장소 서비스 공통 지원 테스트 기반 추가

---
+++ src/test/java/com/typenull/pingdom/moderation/application/service/place/quality/AdminPlaceServiceSupportTest.java
@@ -0,0 +1,7 @@
+package com.typenull.pingdom.moderation.application.service.place.quality;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/** 장소 서비스 공통 변환 함수의 기본 계약을 고정하는 단위 테스트다. */
+class AdminPlaceServiceSupportTest {
+}
