aa9ac47a13ada7faa794162d180a9b9548f39623
yongin1212 <godghr10031@gmail.com>
2026-08-14 23:48:21 +0900

    test : 장소 입력값 정규화 검증 추가

--- src/test/java/com/typenull/pingdom/moderation/application/service/place/quality/AdminPlaceServiceSupportTest.java
+++ src/test/java/com/typenull/pingdom/moderation/application/service/place/quality/AdminPlaceServiceSupportTest.java
@@ -2,6 +2,14 @@ package com.typenull.pingdom.moderation.application.service.place.quality;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Test;
+
 /** 장소 서비스 공통 변환 함수의 기본 계약을 고정하는 단위 테스트다. */
 class AdminPlaceServiceSupportTest {
+
+    @Test
+    void trimToNullNormalizesInput() {
+        assertThat(AdminPlaceServiceSupport.trimToNull("  place  ")).isEqualTo("place");
+        assertThat(AdminPlaceServiceSupport.trimToNull(" ")).isNull();
+    }
 }
