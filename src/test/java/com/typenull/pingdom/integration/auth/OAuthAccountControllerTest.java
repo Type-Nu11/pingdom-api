fae1905b7afc9bad103056c8d918810458f64803
김우성 <azunox9@gmail.com>
2026-08-19 10:27:09 +0900

    refactor : OAuth 통합 테스트 패키지 분리
    실행 환경을 함께 검증하는 테스트를 integration 패키지로 분리했다.

--- src/test/java/com/typenull/pingdom/integration/auth/OAuthAccountControllerTest.java
+++ src/test/java/com/typenull/pingdom/integration/auth/OAuthAccountControllerTest.java
@@ -1,4 +1,4 @@
-package com.typenull.pingdom.auth;
+package com.typenull.pingdom.integration.auth;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
