19ecbc7eff7cc789c8358708606c503a4750f40e
김우성 <azunox9@gmail.com>
2026-08-19 10:26:58 +0900

    refactor : 관리자 게시물 통합 테스트 패키지 분리
    실행 환경을 함께 검증하는 테스트를 integration 패키지로 분리했다.

--- src/test/java/com/typenull/pingdom/integration/admin/AdminPostControllerTest.java
+++ src/test/java/com/typenull/pingdom/integration/admin/AdminPostControllerTest.java
@@ -1,4 +1,4 @@
-package com.typenull.pingdom.admin;
+package com.typenull.pingdom.integration.admin;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
