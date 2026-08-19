0888e91ab357012b006b52aa48e8788fca98962a
김우성 <azunox9@gmail.com>
2026-08-19 10:25:53 +0900

    refactor : 어뷰즈 통합 테스트 패키지 분리
    실행 환경을 함께 검증하는 테스트를 integration 패키지로 분리했다.

--- src/test/java/com/typenull/pingdom/integration/abuse/AbuseRateLimitControllerTest.java
+++ src/test/java/com/typenull/pingdom/integration/abuse/AbuseRateLimitControllerTest.java
@@ -1,4 +1,4 @@
-package com.typenull.pingdom.abuse;
+package com.typenull.pingdom.integration.abuse;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
