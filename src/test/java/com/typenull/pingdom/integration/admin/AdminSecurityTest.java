be0bf86df5a74c5edb5212ed752a0dc5db199394
김우성 <azunox9@gmail.com>
2026-08-19 10:26:58 +0900

    refactor : 관리자 보안 통합 테스트 패키지 분리
    실행 환경을 함께 검증하는 테스트를 integration 패키지로 분리했다.

--- src/test/java/com/typenull/pingdom/integration/admin/AdminSecurityTest.java
+++ src/test/java/com/typenull/pingdom/integration/admin/AdminSecurityTest.java
@@ -1,4 +1,4 @@
-package com.typenull.pingdom.admin;
+package com.typenull.pingdom.integration.admin;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
