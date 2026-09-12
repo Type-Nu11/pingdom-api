d568a6a7a6e7a1e85591894cae7803d71b748236
yongin1212 <godghr10031@gmail.com>
2026-09-12 22:43:45 +0900

    feat : 커뮤니티 게시글 좋아요 기능 추가

---
+++ src/main/java/com/typenull/pingdom/community/domain/CommunityPostLike.java
@@ -0,0 +1,37 @@
+package com.typenull.pingdom.community.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "community_post_like", uniqueConstraints = @UniqueConstraint(
+        name = "uk_community_post_like_post_user", columnNames = {"community_post_id", "user_id"}
+))
+public class CommunityPostLike {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "community_post_like_id")
+    private Long id;
+
+    @Column(name = "community_post_id", nullable = false)
+    private Long communityPostId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
