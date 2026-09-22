01a8992f27e5afa7e12d6aeffe55188161592580
김우성 <azunox9@gmail.com>
2026-09-22 16:44:28 +0900

    docs : DevProfileGuardConfig 처리 책임과 흐름 설명 보강

--- src/main/java/com/typenull/pingdom/shared/config/profile/DevProfileGuardConfig.java
+++ src/main/java/com/typenull/pingdom/shared/config/profile/DevProfileGuardConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+/** dev 프로필 사용 시 별도 허용 플래그가 없으면 시작을 중단해 개발 설정의 우발적 활성화를 막는다. */
 @Configuration(proxyBeanMethods = false)
 @Profile("dev")
 public class DevProfileGuardConfig {
