570bfcf2b85026f10a09ab7566799a0ec246ffcf
김우성 <azunox9@gmail.com>
2026-09-22 16:44:24 +0900

    docs : PlaceRegularOperatingBreakTime·PlaceRegularOperatingHour 처리 책임과 흐름 설명 보강

--- src/main/java/com/typenull/pingdom/place/domain/place/operating/PlaceRegularOperatingBreakTime.java
+++ src/main/java/com/typenull/pingdom/place/domain/place/operating/PlaceRegularOperatingBreakTime.java
@@ -11,6 +11,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 요일별 반복 휴게 구간을 저장하는 값 객체입니다.
+ * 생성자는 전달값을 그대로 보관하며 null·시간 순서 검증을 수행하지 않습니다.
+ */
 @Embeddable
 @Getter
 @EqualsAndHashCode
