d8df9282a0ff08bf9abb8f9ec928c1343a8791ad
김우성 <azunox9@gmail.com>
2026-09-22 16:44:28 +0900

    docs : ErrorCode 처리 책임과 흐름 설명 보강

--- src/main/java/com/typenull/pingdom/shared/exception/ErrorCode.java
+++ src/main/java/com/typenull/pingdom/shared/exception/ErrorCode.java
@@ -2,6 +2,7 @@ package com.typenull.pingdom.shared.exception;
 
 import org.springframework.http.HttpStatus;
 
+/** HTTP 상태·공개 메시지·기계 판독 코드를 제공한다. 기본 getCode 구현은 구현체가 enum이라는 전제를 가진다. */
 public interface ErrorCode {
 
     HttpStatus getStatus();
