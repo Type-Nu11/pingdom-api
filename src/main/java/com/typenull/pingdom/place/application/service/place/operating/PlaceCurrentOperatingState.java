c149914ddf345588eefaef0d90ddffb9a82d8f82
김우성 <azunox9@gmail.com>
2026-09-22 16:44:22 +0900

    docs : MapViewportQueryService·PlaceCurrentOperatingState 처리 책임과 흐름 설명 보강

--- src/main/java/com/typenull/pingdom/place/application/service/place/operating/PlaceCurrentOperatingState.java
+++ src/main/java/com/typenull/pingdom/place/application/service/place/operating/PlaceCurrentOperatingState.java
@@ -2,6 +2,9 @@ package com.typenull.pingdom.place.application.service.place.operating;
 
 import java.time.LocalDateTime;
 
+/**
+ * 영업 일정 평가 시점과 그 시점의 영업 여부입니다. 저장된 장소 운영 상태와 구분되는 계산 결과입니다.
+ */
 public record PlaceCurrentOperatingState(
         boolean currentlyOperating,
         LocalDateTime checkedAt
