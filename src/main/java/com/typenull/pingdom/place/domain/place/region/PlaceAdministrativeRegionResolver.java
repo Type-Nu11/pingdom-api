78dbf968360ed8003c94c3f8e481f64babaafeb5
김우성 <azunox9@gmail.com>
2026-09-22 16:44:25 +0900

    docs : PlaceAdministrativeRegion·PlaceAdministrativeRegionResolver 처리 책임과 흐름 설명 보강

--- src/main/java/com/typenull/pingdom/place/domain/place/region/PlaceAdministrativeRegionResolver.java
+++ src/main/java/com/typenull/pingdom/place/domain/place/region/PlaceAdministrativeRegionResolver.java
@@ -1,5 +1,9 @@
 package com.typenull.pingdom.place.domain.place.region;
 
+/**
+ * 좌표를 시·군·구 정보로 해석하는 외부 제공자 포트입니다.
+ * 호출자는 isConfigured로 사용 가능 여부를 확인할 수 있으며 resolve의 실패 정책은 구현을 따릅니다.
+ */
 public interface PlaceAdministrativeRegionResolver {
 
     boolean isConfigured();
