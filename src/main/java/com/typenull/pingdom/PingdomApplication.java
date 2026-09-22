58f0a8c43db288e4291b27d135c67cdfe06d6bf1
김우성 <azunox9@gmail.com>
2026-09-22 16:44:11 +0900

    docs : PingdomApplication 처리 책임과 흐름 설명 보강

--- src/main/java/com/typenull/pingdom/PingdomApplication.java
+++ src/main/java/com/typenull/pingdom/PingdomApplication.java
@@ -6,11 +6,11 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+/** Spring Boot 애플리케이션의 구성 검색과 실행을 시작하는 진입점입니다. */
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
 @EnableScheduling
-/** Spring Boot 애플리케이션의 구성 검색과 실행을 시작하는 진입점입니다. */
 public class PingdomApplication {
 
     public static void main(String[] args) {
