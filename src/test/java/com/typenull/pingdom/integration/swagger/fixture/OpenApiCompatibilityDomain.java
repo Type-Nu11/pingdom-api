19eb5aaf1f51749116d7ebb8e58101fe43878669
김우성 <azunox9@gmail.com>
2026-09-22 16:44:42 +0900

    refactor : OpenApiCompatibilityDomain 검증 이름과 시나리오 설명 정리

--- src/test/java/com/typenull/pingdom/integration/swagger/fixture/OpenApiCompatibilityDomain.java
+++ src/test/java/com/typenull/pingdom/integration/swagger/fixture/OpenApiCompatibilityDomain.java
@@ -1,5 +1,8 @@
 package com.typenull.pingdom.integration.swagger.fixture;
 
+/**
+ * 호환성 fixture의 문서 그룹을 baseline 파일 이름에 연결한다. WEB은 통합 openapi.json을 사용한다.
+ */
 public enum OpenApiCompatibilityDomain {
     APP("app"),
     COMMON("common"),
@@ -8,10 +11,16 @@ public enum OpenApiCompatibilityDomain {
 
     private final String specName;
 
+    /**
+     * 문서 그룹이 읽을 baseline 파일 이름을 연결한다.
+     */
     OpenApiCompatibilityDomain(String specName) {
         this.specName = specName;
     }
 
+    /**
+     * baseline JSON 파일명에 쓰는 그룹 식별자를 반환한다. WEB은 openapi.json을 가리킨다.
+     */
     public String specName() {
         return specName;
     }
