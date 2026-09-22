bec6e8247b7aa0ef5ba79007783b5feac2d0b4c7
김우성 <azunox9@gmail.com>
2026-09-22 16:44:28 +0900

    docs : AwsRegionProperties·AwsS3Properties 처리 책임과 흐름 설명 보강

--- src/main/java/com/typenull/pingdom/shared/config/aws/AwsS3Properties.java
+++ src/main/java/com/typenull/pingdom/shared/config/aws/AwsS3Properties.java
@@ -2,6 +2,7 @@ package com.typenull.pingdom.shared.config.aws;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+/** S3 bucket 설정을 바인딩한다. 객체 저장소가 실제 작업 시 빈 bucket을 구성 누락으로 처리한다. */
 @ConfigurationProperties(prefix = "spring.cloud.aws.s3")
 public record AwsS3Properties(
         String bucket
