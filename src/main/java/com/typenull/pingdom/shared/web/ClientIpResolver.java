09958bdc0632efe64e7a4ea53ee878d762f2e4e3
김우성 <azunox9@gmail.com>
2026-09-22 16:44:31 +0900

    docs : ClientIpResolver 처리 책임과 흐름 설명 보강

--- src/main/java/com/typenull/pingdom/shared/web/ClientIpResolver.java
+++ src/main/java/com/typenull/pingdom/shared/web/ClientIpResolver.java
@@ -3,6 +3,7 @@ package com.typenull.pingdom.shared.web;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.util.StringUtils;
 
+/** 서블릿 컨테이너가 결정한 원격 주소를 호출 제한 등에 전달한다. forwarded 헤더를 직접 신뢰하지 않는다. */
 public final class ClientIpResolver {
 
     private static final String UNKNOWN_IP = "unknown";
