363b6222689e58461ac577a668ca9b1d89832d71
김우성 <azunox9@gmail.com>
2026-09-22 16:44:24 +0900

    docs : PlaceOperatingException·PlaceOperatingTimeRange 처리 책임과 흐름 설명 보강

--- src/main/java/com/typenull/pingdom/place/domain/place/operating/PlaceOperatingException.java
+++ src/main/java/com/typenull/pingdom/place/domain/place/operating/PlaceOperatingException.java
@@ -23,6 +23,10 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 장소의 특정 날짜에 적용할 휴무 또는 별도 영업시간입니다.
+ * 정규 영업시간보다 우선하며 반환 시간 집합은 복사본입니다. 날짜·시간 조합 유효성은 호출 서비스에서 확인합니다.
+ */
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
--- src/main/java/com/typenull/pingdom/place/domain/place/operating/PlaceOperatingTimeRange.java
+++ src/main/java/com/typenull/pingdom/place/domain/place/operating/PlaceOperatingTimeRange.java
@@ -9,6 +9,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+/**
+ * 날짜별 예외 일정에 사용하는 시작·종료 시각 값입니다.
+ * null 시각을 거부하지만 자정 통과나 두 시각의 동일 여부는 여기서 제한하지 않습니다.
+ */
 @Embeddable
 @Getter
 @EqualsAndHashCode
