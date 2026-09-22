60644a2a11f10eeb4b20cce37e0057f2d07da009
김우성 <azunox9@gmail.com>
2026-09-22 14:58:52 +0900

    docs : Consulting 상담 시작 API 문서 분류 적용

--- src/main/java/com/typenull/pingdom/consultation/api/ConsultationIntroController.java
+++ src/main/java/com/typenull/pingdom/consultation/api/ConsultationIntroController.java
@@ -1,5 +1,8 @@
 package com.typenull.pingdom.consultation.api;
 
+import com.typenull.pingdom.shared.config.swagger.ApiAudience;
+import com.typenull.pingdom.shared.config.swagger.SwaggerTagCatalog;
+
 import com.typenull.pingdom.consultation.api.dto.ConsultationIntroRequest;
 import com.typenull.pingdom.consultation.api.dto.ConsultationIntroResponse;
 import com.typenull.pingdom.consultation.application.ConsultationIntroService;
@@ -18,7 +21,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/consultations")
-@Tag(name = "Consulting", description = "컨설팅 API")
+@ApiAudience(ApiAudience.Group.CONSULTING)
+@Tag(name = SwaggerTagCatalog.INTRO)
 public class ConsultationIntroController {
 
     private final ConsultationIntroService consultationIntroService;
